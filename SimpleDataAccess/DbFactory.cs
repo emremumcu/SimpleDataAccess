cdc02786700b79f991a45b20d4692722f198595c
Emre MUMCU <emumcu@outlook.com>
2019-03-03 22:08:03 +0300

    Add project files.

---
+++ SimpleDataAccess/DbFactory.cs
@@ -0,0 +1,145 @@
+﻿using IBM.Data.DB2;
+using Oracle.ManagedDataAccess.Client;
+using System.Data.Common;
+using System.Data.Odbc;
+using System.Data.OleDb;
+using System.Data.SqlClient;
+
+namespace SimpleDataAccess
+{
+    public enum DbProvider
+    {
+        MSSqlServer, Oracle, IBMDB2, OleDb, Odbc
+    }
+
+    internal class DbFactory
+    {
+        private DbProvider _DbProvider;
+
+        public DbFactory(DbProvider provider)
+        {
+            _DbProvider = provider;
+        }
+
+        public DbProviderFactory CreateFactory()
+        {
+            DbProviderFactory dbFactory = null;
+
+            switch (_DbProvider)
+            {
+                case DbProvider.MSSqlServer:
+                    dbFactory = SqlClientFactory.Instance;
+                    break;
+                case DbProvider.OleDb:
+                    dbFactory = OleDbFactory.Instance;
+                    break;
+                case DbProvider.Oracle:
+                    dbFactory = OracleClientFactory.Instance;
+                    break;
+                case DbProvider.Odbc:
+                    dbFactory = OdbcFactory.Instance;
+                    break;
+                case DbProvider.IBMDB2:
+                    dbFactory = DB2Factory.Instance;
+                    break;
+            }
+
+            return dbFactory;
+        }
+
+        public DbConnection CreateConnection()
+        {
+            switch (_DbProvider)
+            {
+                case DbProvider.MSSqlServer:
+                    return new SqlConnection();
+                case DbProvider.OleDb:
+                    return new OleDbConnection();
+                case DbProvider.Odbc:
+                    return new OdbcConnection();
+                case DbProvider.Oracle:
+                    return new OracleConnection();
+                case DbProvider.IBMDB2:
+                    return new DB2Connection();
+                default:
+                    return null;
+            }
+        }
+
+        public DbConnection CreateConnection(string connectionString)
+        {
+            switch (_DbProvider)
+            {
+                case DbProvider.MSSqlServer:
+                    return new SqlConnection(connectionString);
+                case DbProvider.OleDb:
+                    return new OleDbConnection(connectionString);
+                case DbProvider.Odbc:
+                    return new OdbcConnection(connectionString);
+                case DbProvider.Oracle:
+                    return new OracleConnection(connectionString);
+                case DbProvider.IBMDB2:
+                    return new DB2Connection(connectionString);
+                default:
+                    return null;
+            }
+        }
+
+        public DbCommand CreateCommand()
+        {
+            switch (_DbProvider)
+            {
+                case DbProvider.MSSqlServer:
+                    return new SqlCommand();
+                case DbProvider.OleDb:
+                    return new OleDbCommand();
+                case DbProvider.Odbc:
+                    return new OdbcCommand();
+                case DbProvider.Oracle:
+                    return new OracleCommand();
+                case DbProvider.IBMDB2:
+                    return new DB2Command();
+                default:
+                    return null;
+            }
+        }
+
+        public DbDataAdapter CreateAdpater()
+        {
+            switch (_DbProvider)
+            {
+                case DbProvider.MSSqlServer:
+                    return new SqlDataAdapter();
+                case DbProvider.OleDb:
+                    return new OleDbDataAdapter();
+                case DbProvider.Odbc:
+                    return new OdbcDataAdapter();
+                case DbProvider.Oracle:
+                    return new OracleDataAdapter();
+                case DbProvider.IBMDB2:
+                    return new DB2DataAdapter();
+                default:
+                    return null;
+            }
+        }
+
+        public DbConnectionStringBuilder CreateConnectionStringBuilder()
+        {
+            switch (_DbProvider)
+            {
+                case DbProvider.MSSqlServer:
+                    return new SqlConnectionStringBuilder();
+                case DbProvider.OleDb:
+                    return new OleDbConnectionStringBuilder();
+                case DbProvider.Odbc:
+                    return new OdbcConnectionStringBuilder();
+                case DbProvider.Oracle:
+                    return new OracleConnectionStringBuilder();
+                case DbProvider.IBMDB2:
+                    return new DB2ConnectionStringBuilder();
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ SimpleDataAccess/DbManager.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data;
+
+namespace SimpleDataAccess
+{
+    public partial class DbManager
+    {
+        private DbFactory dbFactory;
+        private DbConnectionStringBuilder dbConnStrBuilder;
+
+        public DbManager(DbProvider provider, string connectionString)
+        {
+            try
+            {
+                dbFactory = new DbFactory(provider);
+                dbConnStrBuilder = dbFactory.CreateConnectionStringBuilder();
+                dbConnStrBuilder.ConnectionString = connectionString;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        #region ExecuteNonQuery
+
+        public int ExecuteNonQuery(string SQL, List<DbParameter> prms = null)
+        {
+            using (DbCommand dbCommand = dbFactory.CreateCommand())
+            {
+                dbCommand.CommandText = SQL;
+                dbCommand.Connection = dbFactory.CreateConnection(dbConnStrBuilder.ConnectionString);
+
+                if (prms != null)
+                {
+                    dbCommand.Parameters.Clear();
+                    dbCommand.Parameters.AddRange(prms.ToArray());
+                }
+
+                dbCommand.Connection.Open();
+                int numRows = dbCommand.ExecuteNonQuery();
+                dbCommand.Connection.Close();
+
+                return numRows;
+            }
+        }
+
+        public int ExecuteNonQuery(string SQL, DbParameter prm)
+        {
+            return ExecuteNonQuery(
+                SQL,
+                new List<DbParameter>() {
+                    prm
+                }
+            );
+        }
+
+        #endregion ExecuteNonQuery
+        
+
+        #region ExecuteScalar
+
+        public object ExecuteScalar(string SQL, List<DbParameter> prms = null)
+        {
+            using (DbCommand dbCommand = dbFactory.CreateCommand())
+            {
+                dbCommand.CommandText = SQL;
+                dbCommand.Connection = dbFactory.CreateConnection(dbConnStrBuilder.ConnectionString); 
+
+                if (prms != null)
+                {
+                    dbCommand.Parameters.Clear();
+                    dbCommand.Parameters.AddRange(prms.ToArray());
+                }
+
+                dbCommand.Connection.Open();
+                object result = dbCommand.ExecuteScalar();
+                dbCommand.Connection.Close();
+
+                return result;
+            }
+        }
+
+        public object ExecuteScalar(string SQL, DbParameter prm)
+        {
+            return ExecuteScalar(
+                SQL,
+                new List<DbParameter>() {
+                    prm
+                }
+            );
+        }
+
+        #endregion ExecuteScalar
+
+
+        #region Select
+
+        public DataTable Select(String SQL, List<DbParameter> prms = null)
+        {
+            using (DbDataAdapter dbDataAdapter = dbFactory.CreateAdpater())
+            {                
+                dbDataAdapter.SelectCommand = dbFactory.CreateCommand();
+                dbDataAdapter.SelectCommand.CommandText = SQL;
+                dbDataAdapter.SelectCommand.Connection = dbFactory.CreateConnection(dbConnStrBuilder.ConnectionString); 
+
+                if (prms != null)
+                {
+                    dbDataAdapter.SelectCommand.Parameters.Clear();
+                    dbDataAdapter.SelectCommand.Parameters.AddRange(prms.ToArray());
+                }
+
+                DataTable returnTable = new DataTable();
+
+                returnTable.BeginLoadData();
+                dbDataAdapter.Fill(returnTable);
+                returnTable.EndLoadData();
+
+                return returnTable;
+            }
+        }
+
+        public DataTable Select(String SQL, DbParameter prm)
+        {
+            return Select(SQL,
+                new List<DbParameter>() {
+                    prm
+                }
+            );
+        }
+
+        #endregion Select
+
+
+
+
+    }
+}
